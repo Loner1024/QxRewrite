971af42765ae0d540524a5e2c1d3acdd66991a1c
Loner1024 <xth12138@gmail.com>
2024-02-21 23:54:25 +0800

    feat:orange first

---
+++ orange.js
@@ -0,0 +1,10 @@
+var body = $response.body;
+var path = $request.path;
+
+var obj = JSON.parse(body);
+
+var data = obj.data
+
+if (path === '/1.0/maleInboxCards/list') {
+    data.map(x => x.userProfile.avatar.picUrl = x.userProfile.avatar.picUrl.replace('/blur/100x200', ''));
+}
