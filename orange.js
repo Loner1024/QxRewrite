aa729756eacd00c7851ea25ea4a910dc37b592cc
Loner1024 <xth12138@gmail.com>
2024-02-22 01:57:42 +0800

    try fix

--- orange.js
+++ orange.js
@@ -2,6 +2,8 @@ var body = $response.body;
 var path = $request.path
 
 var obj = JSON.parse(body);
+console.log(path)
+console.log(path == '/1.0/maleInboxCards/list')
 if (path == '/1.0/maleInboxCards/list') {
     for (let profile of obj.data) {
         profile.userProfile.avatar.picUrl = profile.userProfile.avatar.picUrl.replace('/blur/100x200', '');
