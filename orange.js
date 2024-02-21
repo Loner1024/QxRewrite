ee8bb0c6558f7d03c7d9ab6c1136f49f17ce5ba9
Loner1024 <xth12138@gmail.com>
2024-02-22 01:03:20 +0800

    try fix

--- orange.js
+++ orange.js
@@ -2,16 +2,16 @@ var body = $response.body;
 
 console.log(body)
 
-if (body) {
-	var obj = JSON.parse(body);
-	console.log('哈哈哈哈')
-	console.log(obj)
-	for (let profile of obj.data) {
-		profile.userProfile?.avatar?.picUrl = profile.userProfile.avatar.picUrl.replace('/blur/100x200', '');
-	}
+// if (body) {
+// 	var obj = JSON.parse(body);
+// 	console.log('哈哈哈哈')
+// 	console.log(obj)
+// 	for (let profile of obj.data) {
+// 		profile.userProfile?.avatar?.picUrl = profile.userProfile.avatar.picUrl.replace('/blur/100x200', '');
+// 	}
 
-	console.log(obj)
-}
-console.log(body)
+// 	console.log(obj)
+// }
+// console.log(body)
 
 $done(body);
