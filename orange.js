d14ee6f2837f24f1d77b2ed35e030cde85a840a2
Loner1024 <xth12138@gmail.com>
2024-02-22 00:23:50 +0800

    modify

--- orange.js
+++ orange.js
@@ -1,17 +1,5 @@
 var body = $response.body;
-var path = $request.path;
-
-console.log(path)
-
 var obj = JSON.parse(body);
-
 var data = obj.data
-
-if (path === '/1.0/maleInboxCards/list') {
-	console.log('重写')
-    data.map(x => x.userProfile.avatar.picUrl = x.userProfile.avatar.picUrl.replace('/blur/100x200', ''));
-}
-
-console.log(body)
-
+data.map(x => x.userProfile.avatar.picUrl = x.userProfile.avatar.picUrl.replace('/blur/100x200', ''));
 $done(body);
