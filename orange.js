38f6c1731b9617de851ac8ff8e018af6d8048784
Loner1024 <xth12138@gmail.com>
2024-02-22 00:26:38 +0800

    log

--- orange.js
+++ orange.js
@@ -2,4 +2,8 @@ var body = $response.body;
 var obj = JSON.parse(body);
 var data = obj.data
 data.map(x => x.userProfile.avatar.picUrl = x.userProfile.avatar.picUrl.replace('/blur/100x200', ''));
+
+console.log(data)
+console.log(body)
+
 $done(body);
