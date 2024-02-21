bdb7c1872087a969b36acdf26540db0408baba9c
Loner1024 <xth12138@gmail.com>
2024-02-22 00:55:56 +0800

    try fix

--- orange.js
+++ orange.js
@@ -1,8 +1,11 @@
 var body = $response.body;
 
+console.log(body)
 
 if (body) {
 	var obj = JSON.parse(body);
+	console.log('哈哈哈哈')
+	console.log(obj)
 	for (let profile of obj.data) {
 		profile.userProfile?.avatar?.picUrl = profile.userProfile.avatar.picUrl.replace('/blur/100x200', '');
 	}
