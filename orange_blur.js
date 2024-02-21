98fdbbdc12847f26595597ded03b3e2de70f5cb4
Loner1024 <xth12138@gmail.com>
2024-02-22 03:34:49 +0800

    orange

--- orange_blur.js
+++ orange_blur.js
@@ -6,7 +6,7 @@ var obj = JSON.parse(body);
 
 for (let profile of obj.data) {
     profile.userProfile.avatar.picUrl = profile.userProfile.avatar.picUrl.replace('/blur/100x200', '');
-    profile.profile = 'MatchCard';
+    profile.modelType = 'UserProfile'
     profile.status = 'liked';
 }
 
