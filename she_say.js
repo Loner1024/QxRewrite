355ebd01a4944ed4bfaf4d7ccbe2d1e4f2f49d7b
Loner1024 <xth12138@gmail.com>
2024-06-14 22:22:05 +0800

    refactor:black vip tl

--- she_say.js
+++ she_say.js
@@ -4,6 +4,7 @@ var path = $request.path
 var obj = JSON.parse(body);
 
 obj.privilege.vipTL = 1744293856268;
+obj.privilege.blackVipTL = 1744293856268
 obj.blockGiftCard = true;
 
 var newBody = JSON.stringify(obj);
--- she_say_showusers.js
+++ she_say_showusers.js
@@ -4,6 +4,7 @@ var obj = JSON.parse(body);
 
 obj.isVIP = true
 obj.privilege.vipTL = 1744293856268
+obj.privilege.blackVipTL = 1744293856268
 
 
 var newBody = JSON.stringify(obj);
