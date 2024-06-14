850dc266dbd9507ab282fda2deb08eba58061533
Loner1024 <xth12138@gmail.com>
2024-06-14 22:24:41 +0800

    refactor:blackVipAccessible

--- she_say.js
+++ she_say.js
@@ -5,6 +5,7 @@ var obj = JSON.parse(body);
 
 obj.privilege.vipTL = 1744293856268;
 obj.privilege.blackVipTL = 1744293856268
+obj.blackVipAccessible = false
 obj.blockGiftCard = true;
 
 var newBody = JSON.stringify(obj);
