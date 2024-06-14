bf1730e775c4aa2909969c0a61367503ad6bd279
Loner1024 <xth12138@gmail.com>
2024-06-14 22:51:30 +0800

    show users

--- she_say_showusers.js
+++ she_say_showusers.js
@@ -2,9 +2,21 @@ var body = $response.body;
 
 var obj = JSON.parse(body);
 
+const regex = /album\/([^\/]+)/;
+
 obj.isVIP = true
-// obj.privilege.vipTL = 1744293856268
-// obj.privilege.blackVipTL = 1744293856268
+obj.privilege.vipTL = 1744293856268
+obj.privilege.blackVipTL = 1744293856268
+for (let info of obj.showedInfo) {
+    if (info.uid) {
+        continue
+    }
+    const uid = info.avatar.match(regex);
+    if (uid[1]) {
+        info.uid = uid[1]
+    }
+}
+
 
 var newBody = JSON.stringify(obj);
 
