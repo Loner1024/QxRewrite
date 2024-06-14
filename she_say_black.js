1956a142ba2fcf3192c3e9314e6a3135f93d10d1
Loner1024 <xth12138@gmail.com>
2024-06-14 21:35:50 +0800

    feta:get uid

--- she_say_black.js
+++ she_say_black.js
@@ -2,8 +2,17 @@ var body = $response.body;
 
 var obj = JSON.parse(body);
 
+const regex = /album\/([^\/]+)/;
+
 obj.isBlackVip = true;
 obj.showExposure = true;
+for (let user of obj.userList) {
+    const uid = user.avatar.match(regex);
+    if (uid) {
+        console.log(match[1]);
+    }
+    user.uid = uid
+}
 
 var newBody = JSON.stringify(obj);
 
