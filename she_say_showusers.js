731664bb80d9c2173d57f2e3f06f0b5587692bb3
Loner1024 <xth12138@gmail.com>
2024-06-14 22:29:32 +0800

    refactor:fix mode

--- she_say_showusers.js
+++ she_say_showusers.js
@@ -3,6 +3,10 @@ var reqObj = JSON.parse(reqBody);
 
 reqObj.mode = 2
 
+var newReqBody = JSON.stringify(reqObj);
+
+$done(newReqBody);
+
 var body = $response.body;
 
 var obj = JSON.parse(body);
@@ -10,7 +14,6 @@ var obj = JSON.parse(body);
 obj.isVIP = true
 obj.privilege.vipTL = 1744293856268
 obj.privilege.blackVipTL = 1744293856268
-
 
 var newBody = JSON.stringify(obj);
 
