c4819978391eb18ad361fbc7724987479ecbbcad
Loner1024 <xth12138@gmail.com>
2025-04-11 12:26:41 +0800

    Update orange_blur.js

--- orange_blur.js
+++ orange_blur.js
@@ -5,13 +5,14 @@ var obj = JSON.parse(body);
 
 
 for (let profile of obj.data) {
-    console.log("start")
-    console.log(JSON.stringify(profile))
-    console.log(profile)
-    console.log("end")
-    profile.userProfile.avatar.picUrl = profile.userProfile.avatar.picUrl.replace('/blur/100x200', '');
     profile.modelType = 'UserProfile'
     profile.status = 'liked';
+    try {
+        profile.userProfile.avatar.picUrl = profile.userProfile.avatar.picUrl.replace('/blur/100x200', '');  
+    } catch (e) {
+        console.log(e);
+        continue;
+    }
     if(profile.fuzzy) {
         profile.fuzzy = false;
     }
