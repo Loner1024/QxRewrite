7e5dc4eb0ffcef9bb1231ccd13a0c2ea950cfb6f
Loner1024 <xth12138@gmail.com>
2025-04-11 12:11:29 +0800

    Update orange_blur.js

--- orange_blur.js
+++ orange_blur.js
@@ -5,6 +5,7 @@ var obj = JSON.parse(body);
 
 
 for (let profile of obj.data) {
+    console.log(profile)
     profile.userProfile.avatar.picUrl = profile.userProfile.avatar.picUrl.replace('/blur/100x200', '');
     profile.modelType = 'UserProfile'
     profile.status = 'liked';
